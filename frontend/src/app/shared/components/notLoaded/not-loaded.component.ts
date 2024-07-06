23f821b51efa0b9d26a0bf68b7565d81414bd0a2
Samson Rerri <samson.rerri@gmail.com>
2024-07-06 16:52:19 +0100

    NotLoadedComponent: image position depends on @Input() rightImage variable

--- frontend/src/app/shared/components/notLoaded/not-loaded.component.ts
+++ frontend/src/app/shared/components/notLoaded/not-loaded.component.ts
@@ -12,6 +12,7 @@ export class NotLoadedComponent implements OnInit {
   @Input() noDataFound: boolean = false;
   alertOpt: SweetAlertOptions = {};
   appTitle = '';
+  @Input() rightImage = false;
 
   constructor(public sanitizer: DomSanitizer,) { }
 
